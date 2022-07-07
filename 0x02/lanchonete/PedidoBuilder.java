9cbcf499651c70a4e2077134d4ec5112daa71e36
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-07-07 18:23:52 -0300

    Imprimir task 0

---
+++ 0x02/lanchonete/PedidoBuilder.java
@@ -0,0 +1,44 @@
+public class PedidoBuilder extends PedidoAbstractBuilder {
+
+    private Pedido pedido = new Pedido();
+    public Pedido build() {
+        return pedido;
+    }
+    @Override
+    public void setLanche(TipoLanche tipo) {
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setTipo(TipoItemPedido.LANCHE);
+        itemPedido.setNome(tipo.name());
+
+        pedido.adicionarItemDentroCaixa(itemPedido);
+    }
+
+    @Override
+    public void setBatata(TamanhoBatata tamanho) {
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setTipo(TipoItemPedido.BATATA);
+        itemPedido.setNome(tamanho.name());
+
+        pedido.adicionarItemDentroCaixa(itemPedido);
+    }
+
+    @Override
+    public void setBrinde(TipoBrinde tipo) {
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setTipo(TipoItemPedido.BRINDE);
+        itemPedido.setNome(tipo.name());
+
+        pedido.adicionarItemDentroCaixa(itemPedido);
+    }
+
+    @Override
+    public void setBebida(TipoBebida tipo) {
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setTipo(TipoItemPedido.BEBIDA);
+        itemPedido.setNome(tipo.name());
+
+        pedido.adicionarItemForaCaixa(itemPedido);
+    }
+
+
+}
