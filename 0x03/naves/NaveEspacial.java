5b0485f92fde90fe13be22e0de9faa852b7ad619
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-07-11 11:34:14 -0300

    Imprimir task 0

---
+++ 0x03/naves/NaveEspacial.java
@@ -0,0 +1,20 @@
+public class NaveEspacial {
+    private int saude;
+    private int ataque;
+
+    public NaveEspacial(int saude, int ataque){
+        this.saude = saude;
+        this.ataque = ataque;
+    }
+
+    public int getSaude() {
+        return saude;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public String toString(){
+        return String.format("Saude: %d - Ataque: %d", getSaude(), getAtaque());}
+}
